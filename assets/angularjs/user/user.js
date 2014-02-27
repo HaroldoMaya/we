3d2b63c328591afb6aad48869803c6971332dcc5
Alberto Souza <alberto.souza.99@gmail.com>
2014-02-27 08:58:25 -0300

    added UserService for get and cache users data

--- assets/angularjs/user/user.js
+++ assets/angularjs/user/user.js
@@ -1,3 +1,4 @@
+
 define([
   'angular',
   '$socket',
@@ -68,9 +69,35 @@ define([
     }
   ]);
 
+  // --- SERVICES ---
+  angular.module("application.user")
+  .service("UserService", [
+    "UserResource",
+    function( UserResource ){
+      // users object to store users data
+      var users = {};
+
+      this.users = users;
+
+      this.getUser = function(id){
+        if(users[id]){
+          return this.users[id];
+        }else{
+          users[id] = {};
+
+          return users[id] = UserResource.get({
+            id: id
+          }, function(){
+            return users[id];
+          });
+        }
+      };
+
+    }
+  ]);
+
   // --- RESOURCES ---
   angular.module("application.user")
-  .service("userService", ["$resource"])
   .factory("UserResource", [
     "$resource",
     function ($resource) {
